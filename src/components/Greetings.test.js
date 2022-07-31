c21a8ea408b6fe09040c66bc1f4b14b42523d50d
Daphne <daphneruth304@gmail.com>
2022-07-31 11:45:43 +0300

    updated

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -15,5 +15,18 @@ describe('Greeting component', () => {
     const outputElement = screen.getByText('Changed!');
     expect(outputElement).toBeInTheDocument();
   });
+  test('does not render "good to see you" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const outputElement = screen.queryByText('good to see you', { exact: false });
+    expect(outputElement).toBeNull();
+ });
+});
 
   });
