87d46a71d4e87fca2d35b7ed9f9675db63940efa
Daphne <daphneruth304@gmail.com>
2022-07-31 11:35:21 +0300

    updated

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Hello World as a text', () => {
-  // Arrange
-  render(<Greeting />);
+describe('Greeting component', () => {
+  test('renders Hello World as a text', () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
+    // Act
+    
 
-  // Assert
-  const helloWorldElement = screen.getByText('Hello World!');
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
